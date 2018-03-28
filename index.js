5c49a231f8e120cdead51b54552fea6ebfebc54d
Paul Schwin <schwin42@gmail.com>
2018-03-28 15:40:08 -0700

    Successful setup of js-feat optical flow demo

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
 // app.get('/', (req, res) => {res.sendStatus(200)})
+
+//SSL Support
+const options = {
+    key: fs.readFileSync(__dirname + '/domain.key'),
+    cert: fs.readFileSync(__dirname + '/domain.crt')
+};
+const https = require('https');
+const port = 8080;
+https.createServer(options, app).listen(port);
+
 // app.use(express.static('public'));
 app.use(express.static('js_feat'));
 
-app.listen(3000, () => console.log('Workbench app listening on 3000...'));
+app.listen(function () {
+    console.log('Node workbench listening on ' + port);
+});
